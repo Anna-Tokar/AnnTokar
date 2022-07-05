8257e59cadafaf6756ea896cb761bff9fcf8077d
Anna-Tokar <106422407+Anna-Tokar@users.noreply.github.com>
2022-07-05 18:01:20 +0300

    Разреженный массив на базе двусвязного списка
    Разрежённый массив – массив в котором полезные данные занимают малую часть от его реального размера.  Полезные данные хранятся в структуре индекс-значение,  для всех остальных элементов возвращается значение по умолчанию. 
    Реализовать класс:
    Класс должен содержать поля:
    размер массива;
    значение элемента по умолчанию.
    Класс должен содержать методы:
    получение длины массива;
    получение количества не пустых элементов;
    вывод не пустых элементов массива;
    метод foreach, применяющий функцию обработки для всех не пустых элементов массива.
    Для класса необходимо перегрузить следующие операторы:
    доступа к элементам;
    ввод/вывод в поток.
    
    Используя разрежённый массив вещественных чисел решить задачу:
    Дан массив вещественных чисел большого размера (INT_MAX). Заполнить его случайным количеством элементов, результат  вывести в файл.

--- C++ Разреженный массив на базе двусвязного списка/Source.cpp
+++ C++ Разреженный массив на базе двусвязного списка/Source.cpp
@@ -6,71 +6,67 @@ using namespace std;
 int main() {
 	setlocale(LC_ALL, "Rus");
 
-	//SparseArray M(20);
-	////c = 10;
-	////c = (rand() % (INT_MAX)+1) / 1000; //êîëè÷åñòâî ýëåìåíòîâ
-	////cout << c << endl;
-	//cin >> M;
+	SparseArray M(20);
+	//c = (rand() % (INT_MAX)+1) / 1000; //количество элементов
+	cin >> M;
 
-	//ofstream fin;
-	//fin.open("z2.txt");
-	//int* ind = new int[c];
-	//for (int i = 0; i < c; i++)
-	//	ind[i] = M.operator[](i * 2);
-	//int temp;
-	//for (int i = 1; i < c; i++)
-	//{
-	//	for (int j = 0; j < c - 1; j++)
-	//	{
-	//		if (ind[j] > ind[j + 1])
-	//		{
-	//			temp = ind[j];
-	//			ind[j] = ind[j + 1];
-	//			ind[j + 1] = temp;
-	//		}
-	//	}
-	//}
-	//int i = 0, k = 0;
-	//while (i <= ind[c - 1]) {
-	//	if (k == 0)
-	//		for (int j = 0; j < ind[k]; j++) {
-	//			fin << "0 ";
-	//			i++;
-	//		}
-	//	else
-	//		for (int j = 0; j < (ind[k] - ind[k - 1] - 1); j++) {
-	//			fin << "0 ";
-	//			i++;
-	//		}
-	//	// öèêë äëÿ ïîèñêà â èñõîäíîì ìàññèâå çíà÷åíèÿ ïî èíäåêñó (êîòîðûé ìû ñîðòèðîâàëè âûøå)
-	//	int m = 0; // äëÿ çàïèñè çíà÷åíèÿ
-	//	while (M.operator[](m) != ind[k])
-	//		m = m + 2;
-	//	fin << M.operator[](m + 1) << " ";
-	//	k++;
-	//	i++;
-	//}
-	//for (int j = ind[c - 1] + 1; j < M.size(); j++)
-	//	fin << "0 ";
-	//fin.close();
-
+	ofstream fin;
+	fin.open("z2.txt");
+	int* ind = new int[c];
+	for (int i = 0; i < c; i++)
+		ind[i] = M.operator[](i * 2);
+	int temp;
+	for (int i = 1; i < c; i++)
+	{
+		for (int j = 0; j < c - 1; j++)
+		{
+			if (ind[j] > ind[j + 1])
+			{
+				temp = ind[j];
+				ind[j] = ind[j + 1];
+				ind[j + 1] = temp;
+			}
+		}
+	}
+	int i = 0, k = 0;
+	while (i <= ind[c - 1]) {
+		if (k == 0)
+			for (int j = 0; j < ind[k]; j++) {
+				fin << "0 ";
+				i++;
+			}
+		else
+			for (int j = 0; j < (ind[k] - ind[k - 1] - 1); j++) {
+				fin << "0 ";
+				i++;
+			}
+		// цикл для поиска в исходном массиве значения по индексу (который мы сортировали выше)
+		int m = 0; // для записи значения
+		while (M.operator[](m) != ind[k])
+			m = m + 2;
+		fin << M.operator[](m + 1) << " ";
+		k++;
+		i++;
+	}
+	for (int j = ind[c - 1] + 1; j < M.size(); j++)
+		fin << "0 ";
+	fin.close();
 
 	SparseArray A(10);
-	//c = 5;
-	cout << "Ðàçìåð ìàññèâà:" << A.size() << endl;
-	cout << "Êîëè÷åñòâî íåïóñòûõ ýëåìåíòîâ: " << A.notEmpty() << endl;
+	cout << "Размер массива:" << A.size() << endl;
+	cout << "Количество непустых элементов: " << A.notEmpty() << endl;
 	cin >> A;
-	cout << "Âñå ýëåìåíòû: " << endl;
+	cout << "Все элементы: " << endl;
 	cout << A << endl;
-	cout << "Íåïóñòûå ýëåìåíòû: " << endl;
+	cout << "Непустые элементы: " << endl;
 	A.showElem();
-	cout << "Íåïóñòûå ýëåìåíòû, âîçâåäåííûå â êâàäðàò: " << endl;
+	cout << "Непустые элементы, возведенные в квадрат: " << endl;
 	A.foreach(st);
 	A.showElem();
-	cout << "Íåïóñòûå ýëåìåíòû, ïåðåâåäåííûå â ïîëîæèòåëüíûå: " << endl;
+	cout << "Непустые элементы, переведенные в положительные: " << endl;
 	A.foreach(pos);
 	A.showElem();
-	cout << "Íåïóñòûå ýëåìåíòû, ïåðåâåäåííûå â îòðèöàòåëüíûå: " << endl;
+	cout << "Непустые элементы, переведенные в отрицательные: " << endl;
 	A.foreach(neg);
 	A.showElem();
 }
